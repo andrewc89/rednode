d9ac8226041cc6c7382f9769fdf450a099774744
Andrew Curcie <andrew.curcie@gmail.com>
2013-01-19 17:25:41 +0000

    removed subreddit, added postSelf

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
 
-var Reddit = require("./reddit2");
+var Reddit = require("./reddit");
 var reddit = new Reddit();
 
-reddit.login("testjswrapper", "testjswrapper", function () {
-  reddit.postLink("testjswrapper", "testing rednode", "https://github.com/theyshookhands/Reddit", function (body) {
+reddit.login("GrammarNazism", "wazzup2", function () {
+  //reddit.postSelf("testjswrapper", "testing rednode", "blah blah blah", function (body) {
+    //console.log(body);
+  //});
+  reddit.r("testjswrapper", function (body) {
     console.log(body);
-  });
+  }).top();
 });
--- reddit.js
+++ reddit.js
@@ -1,22 +1,22 @@
 var request = require("request");
+var Subreddit = require("./subreddit");
 
 function Reddit () {
-	this.userAgent = "node.js api wrapper - https://github.com/theyshookhands/rednode";
-	this.cookie = "";
-	this.uh = "";
-	this.username = "";
-	
-	this.getJson = function (url, data, callback) {
-		data["api_type"] = "json";
-		request(url, { qs: data }, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				callback(body);
-			}
-		});
-	};
+  this.userAgent = "node.js api wrapper - https://github.com/theyshookhands/rednode";
+  this.cookie = "";
+  this.uh = "";
 
-	this.post = function (url, data, callback) {
-		data["api_type"] = "json";
+  this.getJson = function (url, data, callback) {
+    data["api_type"] = "json";
+    request(url, { qs: data }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        callback(body);
+      }
+    });
+  };
+
+  this.post = function (url, data, callback) {
+    data["api_type"] = "json";
     if (this.cookie) {
       request.cookie(this.cookie);
     }
@@ -28,45 +28,59 @@ function Reddit () {
         callback(body);
       }
     });
-	};
-
-	this.assignCookies = function (body) {
-		var response = JSON.parse(body);
-    this.uh = response["json"]["data"]["modhash"];
-    this.cookie = response.json.data.cookie;
-    console.log("rednode --> logged in as: " + this.username);
-	};
+  };
 }
 
 Reddit.prototype = {
-	
-	login: function (username, password, callback) {
-		var data = {
-			"user": username,
-			"passwd": password,
-			"rem": false
-		};
-		var self = this;
-		console.log(self.userAgent);
-		this.username = username;
-		this.post("http://www.reddit.com/api/login", data, function (body) {
-			var response = JSON.parse(body);
-			console.log(self.userAgent);
-		  self.uh = response["json"]["data"]["modhash"];
-		  self.cookie = response.json.data.cookie;
-		  console.log("rednode --> logged in as: " + username);
-			callback();		
-		});
-	},
 
-	postLink: function (sr, title, url, callback) {
+  domain: "http://www.reddit.com",
+
+  login: function (username, password, callback) {
+    var data = {
+      "user": username,
+      "passwd": password,
+      "rem": false
+    };
+    var self = this;
+    this.post(this.domain + "/api/login", data, function (body) {
+      var response = JSON.parse(body);
+      self.uh = response["json"]["data"]["modhash"];
+      self.cookie = response.json.data.cookie;
+      console.log("rednode --> logged in as: " + username);
+      callback();
+    });
+  },
+
+  setUserAgent: function (userAgent) {
+    this.userAgent = userAgent;
+  },
+
+  postLink: function (sr, title, url, callback) {
     var data = {
       "kind": "link",
       "sr": sr,
       "title": title,
       "url": url
     };
-    this.post("http://www.reddit.com/api/submit", data, callback);
+    this.post(this.domain + "/api/submit", data, callback);
+  },
+
+  postSelf: function (sr, title, text, callback) {
+    var data = {
+      "kind": "self",
+      "sr": sr,
+      "title": title,
+      "text": text
+    };
+    this.post(this.domain + "/api/submit", data, callback);
+  },
+
+  r: function (subredditName, callback) {
+    var subreddit = new Subreddit(subredditName);
+    if (callback) {
+      subreddit.exec(callback);
+    }
+    return subreddit;
   }
 
 };
